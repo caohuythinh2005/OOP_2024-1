cb2add9603af5c0e0232c20140e1188b392818ce
ThanhQuy78 <lqthanh12072005@gmail.com>
2024-10-22 16:22:12 +0700

    Add files via upload

---
+++ ThanhQuy/KOL.java
@@ -0,0 +1,10 @@
+package ThanhQuy;
+
+public class KOL extends User {
+
+	public KOL(String userName) {
+		super(userName);
+	}
+	 
+}
+	
---
+++ ThanhQuy/Tweet.java
@@ -0,0 +1,45 @@
+package ThanhQuy;
+
+public class Tweet {
+	private String content;
+	private int likes;
+	private int comments;
+	private int retweets;
+	
+	public Tweet(String content ) {
+		this.content=content;
+		this.likes=0;
+		this.comments=0;
+		this.retweets=0;
+	}
+	
+	public void like() {
+		likes++;
+	}
+	
+	public void comment() {
+		comments++;
+	}
+	
+	public void retweet() {
+		retweets++;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public int getLikes() {
+		return likes;
+	}
+
+	public int getComments() {
+		return comments;
+	}
+
+	public int getRetweets() {
+		return retweets;
+	}
+	
+	
+}
